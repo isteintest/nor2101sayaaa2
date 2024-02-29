efe00666164b4a0751dc215514fbe28e2abefd93
isteintest <136920532+isteintest@users.noreply.github.com>
2024-02-29 15:38:58 +0100

    afasfsafsafasfsafsafsaf
    asfasfasfsafsafsafsafsaf

---
+++ 49b7e2bf079caad0694bbabcd1cf17c3.static.js
@@ -0,0 +1,14 @@
+jQuery("#example1").radialProgress("init", {
+  'size': 225,
+  'fill': 10,
+  'text-color': "#fff",
+  'data': [
+    {'color': "#fff", 'perc': 100}
+]
+}).radialProgress("to", {'perc': 100, 'time': 10000});
+
+
+
+
+
+
